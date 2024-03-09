3742595e4d06ca52ac1febe8beac438858f9c970
teavver <jerzyx11@gmail.com>
2024-03-09 11:25:31 +0100

    fix media queries for small devices

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,9 +5,8 @@ import useTheme from "../hooks/useTheme"
 import { useNavigate } from "react-router-dom"
 import { routerPaths } from "../router"
 
-const NavRightList = (props: {themeOnClick: () => void}) => {
-    // TODO: Tidy mobileStyles
-    const mobileStyles = `flex-col max-md:absolute max-md:right-6 max-md:top-14 max-md:p-4 max-md:rounded-xl max-md:border-2 max-md:border-light-border max-md:border-dark-border`
+const NavRightList = (props: { themeOnClick: () => void }) => {
+    const mobileStyles = `flex-col max-md:absolute max-md:right-6 max-md:top-14 max-md:p-4 max-md:rounded-xl max-md:border-2 max-md:border-light-border max-md:dark:border-dark-border max-md:shadow-lg`
     return (
         <div className={`flex justify-center items-center gap-2 bg-light-bgDark dark:bg-dark-bgDark max-md:${mobileStyles}`}>
             <Link text="GitHub" href="https://github.com/el-tumero/graviola" openInNewTab={true} />
@@ -20,7 +19,7 @@ const NavRightList = (props: {themeOnClick: () => void}) => {
 const Navbar = () => {
 
     const navigate = useNavigate()
-    const [,toggleTheme] = useTheme()
+    const [, toggleTheme] = useTheme()
     const [mobileListVisible, setMobileListVisible] = useState<boolean>(false)
 
     return (
@@ -38,8 +37,13 @@ const Navbar = () => {
             </div>
 
             {/* Mobile navbar right panel */}
-            {/* TODO: add icons */}
-            <div className="max-md:visible md:hidden flex w-8 h-8 px-2 bg-blue-500" onClick={() => setMobileListVisible(!mobileListVisible)}>
+            <div className="max-md:visible md:hidden flex items-center w-12 h-max px-2" onClick={() => setMobileListVisible(!mobileListVisible)}>
+                <svg className="h-fit stroke-4 text-light-text dark:text-dark-text" fill="none" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <g id="Menu / Hamburger_MD">
+                        <path id="Vector" d="M5 17H19M5 12H19M5 7H19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </g>
+                </svg>
+
                 {mobileListVisible &&
                     <div className="flex flex-col gap-2">
                         <NavRightList themeOnClick={toggleTheme} />
