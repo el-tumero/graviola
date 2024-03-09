82c351a1332d769ed7e6353b19922f02d31fbd60
teavver <jerzyx11@gmail.com>
2024-03-09 10:50:44 +0100

    remove metamask check (walletconnect)

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,30 @@
+import { useState } from "react"
 import Link from "./Link"
 import Logo from "../assets/logo.webp"
 import useTheme from "../hooks/useTheme"
 import { useNavigate } from "react-router-dom"
 import { routerPaths } from "../router"
 
+const NavRightList = (props: {themeOnClick: () => void}) => {
+    // TODO: Tidy mobileStyles
+    const mobileStyles = `flex-col max-md:absolute max-md:right-6 max-md:top-14 max-md:p-4 max-md:rounded-xl max-md:border-2 max-md:border-light-border max-md:border-dark-border`
+    return (
+        <div className={`flex justify-center items-center gap-2 bg-light-bgDark dark:bg-dark-bgDark max-md:${mobileStyles}`}>
+            <Link text="GitHub" href="https://github.com/el-tumero/graviola" openInNewTab={true} />
+            <button className="hover:underline" onClick={() => props.themeOnClick()}>Theme</button>
+            <w3m-button />
+        </div>
+    )
+}
+
 const Navbar = () => {
 
     const navigate = useNavigate()
     const [,toggleTheme] = useTheme()
+    const [mobileListVisible, setMobileListVisible] = useState<boolean>(false)
 
     return (
-        <div className="sticky z-[1000] bg-light-bgDark dark:bg-dark-bgDark border-b-2 border-light-border dark:border-dark-border top-0 bg-b w-screen py-4 flex justify-between items-center px-[10%] select-none font-content">
+        <div className="sticky z-[1000] bg-light-bgDark dark:bg-dark-bgDark border-b-2 border-light-border dark:border-dark-border top-0 bg-b w-screen py-4 flex justify-between items-center px-[5%] select-none font-content">
 
             <div className="flex justify-center items-center gap-2 cursor-pointer" onClick={() => navigate(routerPaths.root)}>
                 <picture>
@@ -19,10 +33,19 @@ const Navbar = () => {
                 <p className="text-large font-bold hover:underline">GraviolaNFT</p>
             </div>
 
-            <div className="flex justify-center items-center gap-4">
-                <Link text="GitHub" href="https://github.com/el-tumero/graviola" openInNewTab={true} />
-                <button className="hover:underline" onClick={() => toggleTheme()}>Theme</button>
-                <w3m-button />
+            <div className="flex justify-center items-center gap-4 max-md:hidden md:visible">
+                <NavRightList themeOnClick={toggleTheme} />
+            </div>
+
+            {/* Mobile navbar right panel */}
+            {/* TODO: add icons */}
+            <div className="max-md:visible md:hidden flex w-8 h-8 px-2 bg-blue-500" onClick={() => setMobileListVisible(!mobileListVisible)}>
+                {mobileListVisible &&
+                    <div className="flex flex-col gap-2">
+                        <NavRightList themeOnClick={toggleTheme} />
+                    </div>
+                }
+
             </div>
 
         </div>
--- frontend/src/pages/Loading.tsx
+++ frontend/src/pages/Loading.tsx
@@ -1,22 +1,9 @@
-import Link from "../components/Link";
 import FullscreenContainer from "../components/ui/FullscreenContainer";
-import useWeb3 from "../hooks/useWeb3";
-import metamaskLogo from "../assets/metamask.webp";
 
 const Loading = () => {
-    const [web3] = useWeb3()
     return (
         <FullscreenContainer additionalClasses="justify-center font-bold text-2xl">
-            {web3 ?
-                <span>Loading...</span>
-                :
-                <div className="flex justify-center items-center">
-                    <img className="w-32 h-32" src={metamaskLogo} />
-                    <span>
-                        Please install {" "}<Link text="Metamask" href="https://metamask.io/download/" openInNewTab={true} additionalClasses="text-orange-500" />{" "} To view this website.
-                    </span>
-                </div>
-            }
+            <span>Loading...</span>
         </FullscreenContainer>
     )
 }
