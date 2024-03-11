976ab19076c122862281fe8728a43a568fa923fd
teavver <jerzyx11@gmail.com>
2024-03-11 08:01:31 +0100

    breathing anim after generating

--- frontend/src/components/GenerateContainer.tsx
+++ frontend/src/components/GenerateContainer.tsx
@@ -1,33 +1,38 @@
 import { useEffect, useState } from "react"
 import useRandomRarityBorder from "../hooks/useBorderAnimation"
+import { getRarityBorder } from "../utils/getRarityBorder"
+import { RarityLevel } from "../types/Rarity"
 
 interface GenerateContainerProps {
-    imgSrc?: string
+    resImg?: string // Passed only when already rolled
+    resRarity?: RarityLevel // Same here
     isPulsating: boolean
     isGenerating: boolean
 }
 
-const GenerateContainer = ({ imgSrc, isPulsating, isGenerating }: GenerateContainerProps) => {
+const GenerateContainer = ({ resImg, resRarity, isPulsating, isGenerating }: GenerateContainerProps) => {
+
     const rarityAnimBorder = useRandomRarityBorder(isGenerating, 750)
     const [resOpacity, setResOpacity] = useState<number>(0)
+    const resReady = (resImg && resRarity)
+    const { style: breathingBorderStyle = {}, className: breathingBorderClassNames = "" } = resReady ? getRarityBorder(resRarity, true) : {};
 
     useEffect(() => {
-        if (imgSrc) {
+        if (resImg) {
             setResOpacity(0)
             setTimeout(() => setResOpacity(1), 100)
         }
-    }, [imgSrc])
+    }, [resImg])
 
     return (
         // TODO: Change this empty style{} to a passed as prop resultRarity
-        // TODO2: Add pulsating border of rolled rarity color on finish
-        <div style={isGenerating ? rarityAnimBorder : {}} className={`flex justify-center items-center w-64 h-64 p-4 m-8 rounded-xl bg-light-bgDark dark:bg-dark-bgDark border-2 border-light-border dark:border-dark-border ${isPulsating ? "animate-pulse" : ""}`}>
-            {imgSrc &&
+        <div style={resReady ? breathingBorderStyle :isGenerating ? rarityAnimBorder : {}} className={`flex justify-center items-center w-64 h-64 p-4 m-8 rounded-xl bg-light-bgDark dark:bg-dark-bgDark border-2 border-light-border dark:border-dark-border ${resReady ? breathingBorderClassNames : ""} ${isPulsating ? "animate-pulse" : ""}`}>
+            {resImg &&
                 <>
                     <img
                         className={`w-full h-full rounded-lg`}
                         style={{ opacity: resOpacity, transition: 'opacity 0.6s' }}
-                        src={imgSrc} />
+                        src={resImg} />
                 </>
             }
         </div>
--- frontend/src/utils/getRarityBorder.ts
+++ frontend/src/utils/getRarityBorder.ts
@@ -1,16 +1,24 @@
 import { RarityLevel } from "../types/Rarity";
 import { rarities, rarityScale } from "../rarityData";
 
-
 export function getRarityColor(rarityLevel: RarityLevel): React.CSSProperties {
     return { color: rarities[rarityLevel].color }
 }
 
-export function getRarityBorder(rarityLevel: RarityLevel): React.CSSProperties {
-    return {
+export function getRarityBorder(rarityLevel: RarityLevel, breathingEffect?: boolean): { style: React.CSSProperties, className?: string } {
+    const baseStyle = {
         boxShadow: `0px 0px 20px 6px ${rarities[rarityLevel].color}`,
         WebkitBoxShadow: `0px 0px 20px 6px ${rarities[rarityLevel].color}`,
         MozBoxShadow: `0px 0px 20px 6px ${rarities[rarityLevel].color}`,
+        '--rarity-color': rarities[rarityLevel].color,
+    }
+    if (breathingEffect) {
+        return {
+            style: baseStyle,
+            className: 'breathing-effect'
+        }
+    } else {
+        return { style: baseStyle }
     }
 }
 
