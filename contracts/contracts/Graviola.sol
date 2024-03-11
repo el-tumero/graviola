831a096cb4bb6e6194550ad8990f95a7c13d5639
el-tumero <tumerpl@gmail.com>
2024-03-11 18:01:46 +0100

    ownedTokens

--- contracts/contracts/Graviola.sol
+++ contracts/contracts/Graviola.sol
@@ -59,6 +59,8 @@ contract Graviola is ERC721, GraviolaMetadata, GraviolaWell, VRFConsumer, Automa
     // is equal to the id of the next VRF request
     uint256 private _nextVrfReqId;
 
+    // mapping for tokens owned by address (user)
+    mapping(address => uint256[]) private _ownedTokens;
 
     /// @notice creates the ERC-721 token contract that uses verifiable randomness and on-chain AI oracles
     /// @param aiOracle address of the on-chain AI oracle contract
@@ -149,6 +151,41 @@ contract Graviola is ERC721, GraviolaMetadata, GraviolaWell, VRFConsumer, Automa
             emit TokenReady(address(0), id);
         }
     }
+
+    function debugMint(address to) external {
+        _safeMint(to, _nextTokenId);
+        _nextTokenId++; 
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
+        address previousOwner = super._update(to, tokenId, auth);
+
+        if(to != address(0)) _ownedTokens[to].push(tokenId);
+        return previousOwner;
+    }
+
+
+
+    function ownedTokens(address addr) public view returns (uint256[] memory output){
+        uint256[] memory buffer = new uint256[](_ownedTokens[addr].length);
+        uint256 j = 0;
+        for (uint i = 0; i < _ownedTokens[addr].length; i++) {
+            uint256 tokenId = _ownedTokens[addr][i];
+            if(_ownerOf(tokenId) == addr){
+                buffer[j] = tokenId;
+                j++;
+            }
+        }
+        output = new uint256[](j);
+        for (uint i = 0; i < j; i++) {
+            output[i] = buffer[i];
+        }
+    }
+
+    function burn(uint256 tokenId) external {
+        _burn(tokenId);
+    } 
+
 
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory){
         return _tokenURI(tokenId);
