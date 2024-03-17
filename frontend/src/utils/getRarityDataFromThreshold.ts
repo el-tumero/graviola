2865492532154329d49c6a1f8db90a6aa9fba112
teavver <jerzyx11@gmail.com>
2024-03-17 19:22:26 +0100

    fix rarity percentage util function

--- frontend/src/utils/getRarityDataFromThreshold.ts
+++ frontend/src/utils/getRarityDataFromThreshold.ts
@@ -21,25 +21,11 @@ export function getRarityFromThreshold(threshold: number): [RarityLevel, RarityD
             }
         }
 
-    // if (threshold > 100) threshold = 100
-    // if (threshold < 0) threshold = 0
-    // for (let rarityLevel of rarityScale) {
-    //     const rarityData = rarities[rarityLevel]
-    //     if (threshold >= rarityData.threshold) {
-    //         return [rarityLevel, rarityData]
-    //     }
-    // }
     return [RarityLevel.Legendary, rarities[RarityLevel.Legendary]] // This is here just for the return type
 }
 
 // Get rarityLevel and rarityData from BP value, e.g. (1499 = 1,499%, 28230 = 28,230%)
 export function formatBpToPercentage(bp: number): number {
-    let divisor = 100000
-    let length = bp.toString().length
-    if (length <= 5) {
-        divisor = 1000
-    } else if (length >= 6) {
-        divisor = 10000
-    }
+    let divisor = 10000
     return bp / divisor
 }
