942e327f37fe76d3a0da1170ed72bf016aea56e9
teavver <jerzyx11@gmail.com>
2024-03-17 18:17:45 +0100

    update GraviolaWell docs, better naming

--- contracts/contracts/GraviolaWell.sol
+++ contracts/contracts/GraviolaWell.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
+/// @notice Contract for rolling Graviola tokens and all logic related to keywords
 contract GraviolaWell {
+
     // Internal Word structure
+    // Each Word has its rarity, calculated by: upperRange / lowerRange
+    // The Word's rarity determines how likely it is to roll the keyword
     struct Word {
         string keyword;
         uint256 lowerRange;
@@ -12,89 +16,58 @@ contract GraviolaWell {
     }
 
     // Struct for displaying human-readable info about keywords
+    // Used in frontend calls where Word ranges are useless, but percentages matter
     struct WordStats {
         string keyword;
         uint256 rarityPerc;
     }
 
+    // Each keyword needs its id
     mapping(string => uint256) internal wordMap;
     Word[] public WELL_OF_WORDS;
-    uint256 public WELL_OF_WORDS_MIN_R = 10; // Minimum rarity for any keyword
-    uint256 public WELL_OF_WORDS_TOTAL_R = 2000; // Collective rarity of all keywords
-    uint256 public KEYWORDS_PER_TOKEN = 3; // How many keywords should be used to roll and generate the image
+
+    uint256 public WELL_OF_WORDS_MIN_R = 1; // Minimum rarity for any keyword
+    uint256 public WELL_OF_WORDS_TOTAL_R = 2021; // Collective rarity of all base* keywords
+    // *Base keywords = all Words included with the contract. Any keyword added by a User is not a base keyword
+
+    uint256 public constant KEYWORDS_PER_TOKEN = 3; // How many keywords should determine the token's description (result)
 
     event RollResult(string result, uint256 rarity);
 
     constructor() {
-        // Init base keywords and rarity factors
-        // Simplified for probability testing
 
+        // Init base keywords and rarity factors
         WELL_OF_WORDS.push(Word("human", 0, 999));
         wordMap["human"] = 0;
-
         WELL_OF_WORDS.push(Word("goblin", 1000, 1249));
         wordMap["goblin"] = 1;
-
         WELL_OF_WORDS.push(Word("alien", 1250, 1749));
         wordMap["alien"] = 2;
-
         WELL_OF_WORDS.push(Word("elf", 1750, 1999));
         wordMap["elf"] = 3;
-
-        // WELL_OF_WORDS.push(Word("human", 0, 999));
-        // WELL_OF_WORDS.push(Word("elf", 1000, 1249));
-        // WELL_OF_WORDS.push(Word("bald", 1250, 1349));
         WELL_OF_WORDS.push(Word("hairy", 2000, 2019));
         wordMap["hairy"] = 4;
-
         WELL_OF_WORDS.push(Word("monobrow", 2020, 2021));
         wordMap["monobrow"] = 5;
-        
-        // WELL_OF_WORDS.push(Word("tattoo", 1400, 1449));
-        // WELL_OF_WORDS.push(Word("green", 1450, 1499));
-        // WELL_OF_WORDS.push(Word("red", 1500, 1599));
-        // WELL_OF_WORDS.push(Word("blue", 1600, 1749));
-        // WELL_OF_WORDS.push(Word("goblin", 1750, 1949));
-        // WELL_OF_WORDS.push(Word("android", 1950, 1999));
-        // WELL_OF_WORDS.push(Word("cyborg", 2000, 2009));
-        // WELL_OF_WORDS.push(Word("android", 2010, 2012));
-        // WELL_OF_WORDS.push(Word("mage", 2013, 2062));
-        // WELL_OF_WORDS.push(Word("angry", 2063, 2115));
-        // WELL_OF_WORDS.push(Word("stunned", 2116, 2205));
-        // WELL_OF_WORDS.push(Word("monobrow", 2206, 2207));
 
-        // WELL_OF_WORDS.push(Word("human", 1500));
-        // WELL_OF_WORDS.push(Word("elf", 250));
-        // WELL_OF_WORDS.push(Word("goblin", 150));
-        // WELL_OF_WORDS.push(Word("android", 100));
-
-        // WELL_OF_WORDS.push(Word("piercing", 20));
-        // WELL_OF_WORDS.push(Word("bald", 10));
-        // WELL_OF_WORDS.push(Word("tattoo", 30));
-        // WELL_OF_WORDS.push(Word("hairy", 5));
-        // WELL_OF_WORDS.push(Word("white", 200));
-        // WELL_OF_WORDS.push(Word("green", 200));
-        // WELL_OF_WORDS.push(Word("black", 200));
-        // WELL_OF_WORDS.push(Word("red", 200));
-        // WELL_OF_WORDS.push(Word("blue", 200));
-        // WELL_OF_WORDS.push(Word("yellow", 200));
     }
 
     /// @notice Add a new keyword to the pool (well) of possible keywords.
     /// @notice The rarity of new keywords is chosen randomly.
     function addWordToWell(string memory _keyword, uint256 _seed) public {
-        // TODO: replace _seed with VRF call
-        // Reject if caller does not own at least one NFT
+
+        // TODO: Reject if caller does not own at least one NFT
+
         // Keyword should be no longer than 12 characters
         require(bytes(_keyword).length <= 12 && bytes(_keyword).length > 0);
 
         // Generate a (pseudorandom) probability for the new keyword
-        uint256 rand = uint256(keccak256(abi.encodePacked(_seed, msg.sender))); // CHANGE THIS TO VRF
+        uint256 rand = uint256(keccak256(abi.encodePacked(_seed, msg.sender))); // TODO: CHANGE THIS TO VRF
         uint256 newWordRange = ((rand %
             ((WELL_OF_WORDS_TOTAL_R - WELL_OF_WORDS_MIN_R) / 10 + 1)) * 10) +
             WELL_OF_WORDS_MIN_R;
 
-        // Add probability sum to totalRarity
+        // Add new keyword's probability (range sum) to totalRarity
         WELL_OF_WORDS_TOTAL_R += newWordRange;
 
         Word memory currentLastWord = WELL_OF_WORDS[WELL_OF_WORDS.length - 1];
@@ -113,13 +86,10 @@ contract GraviolaWell {
     /// @notice We're estimating the word's probability with this equation: 1 - (1 - P)^n,
     /// @notice Where `P` is the probability of target keyword in a single keyword roll and `n` is the KEYWORDS_PER_TOKEN value.
     function calculateEstimatedWordRarityPerc(string memory _keyword) public view returns (uint256) {
-        uint rawKeywordRange = WELL_OF_WORDS[wordMap[_keyword]].upperRange - WELL_OF_WORDS[wordMap[_keyword]].lowerRange;
-        // (1-P) operation; for greater division precision, the number 10_000 is used 
-        uint singleRollProbability = 10_000 - ((rawKeywordRange*10_000)/WELL_OF_WORDS_TOTAL_R);
-        // (1-P)^n
-        uint totalRollProbability = singleRollProbability ** KEYWORDS_PER_TOKEN;
-        // 1 - (1-P)^n operation; 1e12 = (10_000)^3; 1e6 is used to decrease precision of the result
-        return (1e12 - totalRollProbability) / 1e6;
+        uint keywordRangeSum = WELL_OF_WORDS[wordMap[_keyword]].upperRange - WELL_OF_WORDS[wordMap[_keyword]].lowerRange;
+        uint singleRollProbability = 10_000 - ((keywordRangeSum * 10_000) / WELL_OF_WORDS_TOTAL_R); // (1-P) operation; for greater division precision, the number 10_000 is used 
+        uint totalRollProbability = singleRollProbability ** KEYWORDS_PER_TOKEN;                    // (1-P)^n
+        return (1e12 - totalRollProbability) / 1e6;                                                 // 1 - (1-P)^n operation; 1e12 = (10_000)^3; 1e6 is used to decrease precision of the result
     }
 
     function getWellOfWords() public view returns (Word[] memory) {
@@ -145,6 +115,7 @@ contract GraviolaWell {
         return (numerator * 100) / denumerator;
     }
 
+    /// @notice Find the keyword (index in WELL_OF_WORDS) from integer
     function findNearestWordRangeIndex(
         uint256 _number
     ) public view returns (uint256) {
@@ -166,20 +137,22 @@ contract GraviolaWell {
         }
     }
 
+    /// @notice Roll 3 random keywords based on VRF (used for Token generation later)
     function rollWords(
         uint256 _seed
     ) public view returns (string memory, uint256) {
-        uint8 keywordAmount = 3;
-        uint256 totalProbability;
-        string memory result = "";
-        int256[3] memory usedRangeIndices = [int256(-1), -1, -1];
-        uint256[3] memory bpProbabilities;
-        uint256 dynamicRarity = WELL_OF_WORDS_TOTAL_R; // Dynamic rarity of each iteration
+
+        uint256 rollProbability;                         // Cumulative probability of all keywords in a single roll (in BP)
+        uint256 rollTotalRarity = WELL_OF_WORDS_TOTAL_R; // Make a copy of totalRarity to keep track of relative probabilities
+        string memory result = "";                       // Concatenated rolled keywords
+
+        int256[KEYWORDS_PER_TOKEN] memory usedRangeIndices = [int256(-1), -1, -1];
+        uint256[KEYWORDS_PER_TOKEN] memory bpProbabilities;
 
         uint256 i = 0;
         uint256 j = 0;
 
-        while (i < keywordAmount) {
+        while (i < KEYWORDS_PER_TOKEN) {
             j++;
 
             uint256 randomNum = uint256(
@@ -191,12 +164,12 @@ contract GraviolaWell {
                         block.timestamp
                     )
                 )
-            ) % WELL_OF_WORDS_TOTAL_R; // Random number between [0 and totalRarity]
+            ) % WELL_OF_WORDS_TOTAL_R; // Roll random number between [0 and totalRarity]
             uint256 randomNumWordRangeIndex = findNearestWordRangeIndex(
                 randomNum
             );
 
-            // If we rolled a word (range) that has been already used -> roll again.
+            // If we rolled a word (integer in range) that has been already used, we roll again
             if (
                 (usedRangeIndices[0] == int256(randomNumWordRangeIndex)) ||
                 (usedRangeIndices[1] == int256(randomNumWordRangeIndex)) ||
@@ -205,17 +178,17 @@ contract GraviolaWell {
                 continue;
             }
 
-            // Calculate probability factor of selected keyword & subtract from total probability.
+            // Calculate probability factor of selected keyword & subtract from total probability
             uint256 selectedWordTotalRarity = WELL_OF_WORDS[
                 randomNumWordRangeIndex
             ].upperRange - WELL_OF_WORDS[randomNumWordRangeIndex].lowerRange;
 
             bpProbabilities[i] = (
-                fractionToBasisPoints(selectedWordTotalRarity, dynamicRarity)
+                fractionToBasisPoints(selectedWordTotalRarity, rollTotalRarity)
             );
-            dynamicRarity -= selectedWordTotalRarity; // Update dynamic rarity
+            rollTotalRarity -= selectedWordTotalRarity; // Update dynamic rarity
 
-            // // Add selected word's range to usedRangeIndices to prevent duplicates.
+            // Add selected word's index to usedRangeIndices (duplicate filter)
             usedRangeIndices[i] = int256(randomNumWordRangeIndex);
 
             result = string(
@@ -229,9 +202,9 @@ contract GraviolaWell {
             i++;
         }
 
-        totalProbability = (bpProbabilities[0] *
+        rollProbability = (bpProbabilities[0] *
             bpProbabilities[1] *
             bpProbabilities[2]);
-        return (result, totalProbability);
+        return (result, rollProbability);
     }
 }
