647f3aa687c47493bc28bcb32b35fcc7225b88bb
el-tumero <tumerpl@gmail.com>
2024-04-28 14:07:32 +0200

    build fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,13 @@ export default defineConfig(() => {
         plugins: [react(), nodePolyfills({
             include: ['util']
         })],
-        base: process.env.NODE_ENV === "dev" ? "/" : "/graviola/"
+        base: process.env.NODE_ENV === "dev" ? "/" : "/graviola/",
+        build: {
+            rollupOptions: {
+                external: [
+                    "vite-plugin-node-polyfills/shims/global"
+                ]
+            }
+        }
     }
 })
