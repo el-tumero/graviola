7aba1a532575f61fdcbbb33f8238a313a1e6a3c5
el-tumero <tumer.pl@gmail.com>
2024-06-15 15:59:35 +0200

    SeasonsArchive added

--- contracts/src/Graviola/seasons/GraviolaSeasonsArchive.sol
+++ contracts/src/Graviola/seasons/GraviolaSeasonsArchive.sol
@@ -2,45 +2,69 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IGraviolaSeasonsArchive.sol";
 
-contract GraviolaSeasonsArchive is Ownable{
+contract GraviolaSeasonsArchive is Ownable, IGraviolaSeasonsArchive{
 
     constructor(address owner) Ownable(owner){}
-    
+
+    uint16 wellSize = 100; // how many words can be added to single season's well
+    uint8 expLen = 8; // how many words expire after season
+
+    uint8 constant totalRarityGroups = 5;
+
     enum Rarity{ COMMON, UNCOMMON, RARE, VERY_RARE, LEGENDARY }
 
-    struct WordDetails {
-        Rarity rarity;
-        bool expired;
-    }
-
-
-    struct Season {
-        string name;
-        string[] well; // TODO: soon will be struct or array of structs
-    }
-
-  
-
-    mapping(string=>WordDetails) private words;
+    mapping(string=>bool) private expiredWords;
+    mapping(string=>bool) private usedWords;
     mapping(uint256=>Season) private seasons;
 
     uint256 private currentSeasonId;
+    uint8[totalRarityGroups] wordsPerRarityGroup = [77, 15, 5, 2, 1]; 
     
-    function nameSeason(uint seasonId, string calldata name) external onlyOwner {
+    
+    function nameSeason(uint256 seasonId, string calldata name) external onlyOwner {
         seasons[seasonId].name = name;
     }
 
-    function switchSeason() external onlyOwner {
+    function nextSeason() external onlyOwner {
         ++currentSeasonId;
     }
 
-    function addWordToSeason(uint seasonId, string calldata word) external onlyOwner {
-        // seasons[seasonId].well.push(word);
-
+    function getSeason(uint256 seasonId) external view returns (Season memory) {
+        return seasons[seasonId];
     }
 
-    function checkWordDetails(string calldata word) external view returns (WordDetails memory) {
-        return words[word];
+    function getCurrentSeasonId() external view returns (uint256) {
+        return currentSeasonId;
+    }
+
+
+    function addWordToSeason(uint seasonId, string calldata word) external onlyOwner {
+        Season storage season = seasons[seasonId];
+        require(season.well.length < wellSize);        
+        season.well.push(word);
+        if(season.well.length <= expLen) expiredWords[word] = true;
+        else usedWords[word] = true;
+    }
+
+    function isWordExpired(string calldata word) external view returns (bool) {
+        return expiredWords[word];
+    }
+
+    function isWordUsed(string calldata word) external view returns (bool) {
+        return usedWords[word];
+    }
+
+    function getWordsPerRarityGroup() external view returns (uint8[totalRarityGroups] memory) {
+        return wordsPerRarityGroup;
+    }
+
+    function setWellSize(uint16 size) external onlyOwner {
+        wellSize = size;
+    }
+
+    function setExpLen(uint8 len) external onlyOwner {
+        expLen = len;
     }
 }
--- contracts/src/Graviola/seasons/GraviolaSeasonsGovernor.sol
+++ contracts/src/Graviola/seasons/GraviolaSeasonsGovernor.sol
@@ -1,12 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "./IGraviolaSeasonsArchive.sol";
+
 contract GraviolaSeasonsGovernor{
 
+    error WrongWordFormat();
+    error WordExpired();
+
+    IGraviolaSeasonsArchive immutable archive;
+
+    constructor(address archiveAddress){
+        archive = IGraviolaSeasonsArchive(archiveAddress);
+    }
+
+    uint256 private currentVotingId = 0;
+
+    mapping(uint256=>Voting) private votings;
+
+    struct Voting {
+        mapping(string=>int256) votes;
+        mapping(string=>bool) nominated;
+        string[] oldCandidates;
+        string[] newCandidates;
+    }
+
+    function isCandidate(string calldata word, uint256 votingId) external view returns (bool) {
+        return votings[votingId].nominated[word];
+    }
 
 
-    function proposeWord() external {
+    function validateWord(string calldata str) private pure returns (bool) {
+        bytes memory b = bytes(str);
+        if(b.length > 20 || b.length == 0) return false;
+        for (uint256 i = 0; i < b.length; i++) {
+            uint8 char = uint8(b[i]);
+            if(char < 97 || char > 122) return false;            
+        }
+        return true;
+    }
+
+    function voteForWord(string calldata word, uint256 votingPower, bool isUpvote) external {
+        if (!validateWord(word)) revert WrongWordFormat();
+        if(archive.isWordExpired(word)) revert WordExpired();
         
+        Voting storage currentVoting = votings[currentVotingId];
+
+        if(!currentVoting.nominated[word]) {
+            if(archive.isWordUsed(word)) currentVoting.oldCandidates.push(word);
+            else currentVoting.newCandidates.push(word);
+            currentVoting.nominated[word] = true;
+        }
+
+        if(isUpvote) currentVoting.votes[word] = currentVoting.votes[word] + int256(votingPower); // TODO: check overflow
+        else currentVoting.votes[word] = currentVoting.votes[word] - int256(votingPower);
+
+    }   
+
+
+    function getCurrentVotingId() external view returns(uint256) {
+        return currentVotingId;
     }
 
 }
