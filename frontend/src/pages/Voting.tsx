fcdf251123f1c80fe4c95d3222dabaa5d618f0d7
teavver <jerzyx11@gmail.com>
2024-07-17 19:56:58 +0200

    good layout

--- frontend/src/pages/Voting.tsx
+++ frontend/src/pages/Voting.tsx
@@ -4,9 +4,23 @@ import ContentContainer from "../components/ui/layout/ContentContainer"
 import FullscreenContainer from "../components/ui/layout/FullscreenContainer"
 import PageTitle from "../components/ui/layout/PageTitle"
 import SectionContainer from "../components/ui/layout/SectionContainer"
-import { useState } from "react"
+import candidateMockList from "../../../contracts/candidates.json"
+import { Fragment, useContext, useState } from "react"
+import { getKeyword } from "../utils/getKeyword"
+import { GraviolaContext } from "../contexts/GraviolaContext"
+import { RarityGroupData } from "../types/Rarity"
+import { RaritiesData } from "../types/RarityGroup"
 
 type ActivePage = "Voting" | "Archive"
+
+interface CandidateInfo {
+    id: number
+    badge?: null // Later
+    author: string
+    keyword: string
+    iteration: number
+    score: number
+}
 
 const Voting = () => {
 
@@ -54,7 +68,7 @@ const Voting = () => {
                 </SectionContainer>
 
                 {activePage === "Voting"
-                    ? <KeywordVotingPage /> : <></>
+                    ? <KeywordVotingPage /> : <ArchivePage />
                 }
 
             </ContentContainer>
@@ -63,54 +77,77 @@ const Voting = () => {
     )
 }
 
+const ArchivePage = () => <p className="self-center">Soon</p>
+
 const KeywordVotingPage = () => {
+
+    const { rarities } = useContext(GraviolaContext) as {
+        rarities: RaritiesData
+    }
+
+    const mockCandidates = candidateMockList.map((data, i) => {
+        const candidateData: CandidateInfo = {
+            id: i,
+            author: data[2],
+            keyword: getKeyword(+data[0], rarities)[0],
+            iteration: 0,
+            score: +data[1],
+        }
+        return candidateData
+    })
+
     return (
         <div className={cl(
-            "flex flex-col justify-start items-start w-3/4 h-fit self-center",
+            "flex flex-col justify-start items-start w-1/2 h-fit self-center",
             "border border-light-border dark:border-dark-border rounded-lg p-1",
         )}>
-
             <ul role="list" className="w-full divide-y divide-light-border dark:divide-dark-border">
-                <KeywordCandidate />
-                <KeywordCandidate />
-                <KeywordCandidate />
-                <KeywordCandidate />
-                <KeywordCandidate />
-                <KeywordCandidate />
+                {mockCandidates.map((candInfo, idx) => (
+                    <KeywordCandidate {...candInfo} key={idx} />
+                ))}
             </ul>
-
-
         </div>
     )
 }
 
-const KeywordCandidate = () => (
+const KeywordCandidate = (props: CandidateInfo) => (
     <div className={cl(
-        "flex w-full h-fit px-1.5 py-3 justify-between items-center overflow-x-hidden",
+        "flex w-full h-fit p-3 justify-between items-center overflow-x-hidden",
         "max-lg:flex-col max-lg:justify-start max-lg:items-start",
-        "even:bg-light-bgDark/20 even:dark:bg-dark-bgLight/20",
-        "odd:bg-light-bgDark/40 odd:dark:bg-dark-bgLight/40",
+        "even:bg-light-border/30 even:dark:bg-dark-border/30",
+        "odd:bg-light-bgDark/30 odd:dark:bg-dark-bgLight/30",
         "first:rounded-t-md last:rounded-b-md"
     )}>
-        {/* Left part - badge + keyword container (with iteration info) */}
-        <div className={cl("flex flex-nowrap gap-1 justify-center items-center w-2/3 h-full")}>
-            <div className="w-8 h-8 flex justify-center items-center bg-gray-400/50">
-                🔥
+        {/* Left part: Id, Badge, Keyword, Iteration info */}
+        <div className="mr-2">
+            <span className="font-mono text-light-text/75 dark:text-dark-text/75">{props.id}.</span>
+        </div>
+        <div className={cl("flex flex-nowrap gap-3 justify-center items-center w-2/3 h-full")}>
+            <div className={cl(
+                "w-8 h-8 flex justify-center items-center",
+                !props.badge && ["border-2 border-light-border dark:border-dark-border", "border-dashed rounded-md"]
+            )}>
+                {props.badge && <img src={props.badge} />}
             </div>
             <div className="flex w-full flex-1 gap-0.5 justify-start items-center">
-                <p>WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW</p>
-                <p className="font-mono text-[10px] mb-1.5">(3)</p>
+                <p>{props.keyword}</p>
+                <p className="font-mono text-light-text/75 dark:text-dark-text/75 text-[10px] mb-1.5">
+                    ({props.iteration})
+                </p>
             </div>
         </div>
-        {/* Right part - Score + upvote, downvote */}
+        {/* Right part: Score, Upvote, Downvote */}
         <div className={cl(
             "w-1/3 flex justify-between items-center h-full gap-3"
         )}>
             <div className={cl("flex flex-1 justify-end items-center")}>
-                <p className="text-gray-600">Score: 240</p>
+                <p className="text-gray-600">
+                    <span className="text-xs text-light-text/75 dark:text-dark-text/75 font-mono">score: </span>
+                    <span className="font-semibold">{props.score}</span>
+                </p>
             </div>
-            <div className={cl("w-8 h-8 flex justify-center items-center bg-gray-400/50")}>{'\u2191'}</div>
-            <div className={cl("w-8 h-8 flex justify-center items-center bg-gray-400/50")}>{'\u2193'}</div>
+            <div className={cl("rounded-lg w-8 h-8 flex justify-center items-center bg-gray-400/50")}>{'\u2191'}</div>
+            <div className={cl("rounded-lg w-8 h-8 flex justify-center items-center bg-gray-400/50")}>{'\u2193'}</div>
         </div>
     </div>
 )
