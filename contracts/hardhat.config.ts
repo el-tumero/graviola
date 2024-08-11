d76ff8dd501a15f72ef499447d8dcf0270c98abb
el-tumero <tumerpl@gmail.com>
2024-08-12 00:12:38 +0200

    solhint setup

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,30 +1,31 @@
-import "dotenv/config"
-import { HardhatUserConfig } from "hardhat/config"
-import "@nomicfoundation/hardhat-toolbox"
+import 'dotenv/config'
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import '@nomiclabs/hardhat-solhint'
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const API_KEY_INFURA = process.env.API_KEY_INFURA
 
 const config: HardhatUserConfig = {
-    networks: {
-        hardhat: {
-            blockGasLimit: 300_000_000,
-            allowUnlimitedContractSize: true,
-        },
-        testnet: {
-            url: `https://sepolia.infura.io/v3/${API_KEY_INFURA}`,
-            accounts: PRIVATE_KEY ? [PRIVATE_KEY] : undefined,
-        },
+  networks: {
+    hardhat: {
+      blockGasLimit: 300_000_000,
+      allowUnlimitedContractSize: true,
     },
-    solidity: {
-        version: "0.8.24",
+    testnet: {
+      url: `https://sepolia.infura.io/v3/${API_KEY_INFURA}`,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : undefined,
     },
-    paths: {
-        sources: "./src",
-        tests: "./test",
-        cache: "./cache_hardhat",
-        artifacts: "./artifacts",
-    },
+  },
+  solidity: {
+    version: '0.8.24',
+  },
+  paths: {
+    sources: './src',
+    tests: './test',
+    cache: './cache_hardhat',
+    artifacts: './artifacts',
+  },
 }
 
 export default config
