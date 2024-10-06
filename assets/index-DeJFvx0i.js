dbc7b7d61f0bebc76e7ef95fff50a0e9fc28699c
el-tumero <tumerpl@gmail.com>
2024-10-06 14:14:39 +0200

    Updates

--- assets/index-DeJFvx0i.js
+++ assets/index-DeJFvx0i.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-Cq55YyJI.js";import{j as Ur}from"./index-QP6Qw1JF.js";/**
+import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-KS2vq2wL.js";import{j as Ur}from"./index-CvfW4Qvg.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
