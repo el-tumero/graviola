2f17a7d541edc5affe04d2608005ab9bf9abb8b5
el-tumero <tumerpl@gmail.com>
2024-10-06 12:09:33 +0200

    Updates

--- assets/index-bx2c-c3l.js
+++ assets/index-bx2c-c3l.js
@@ -1,4 +1,4 @@
-import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-DTO05Xgn.js";import{j as Ur}from"./index-CVAtE-6s.js";/**
+import{n as fe,s as Ke,T as O,t as L,a as g,o as Y,R as Nr,p as H,y as ae}from"./index-Cq55YyJI.js";import{j as Ur}from"./index-QP6Qw1JF.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
