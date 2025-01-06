1026e32f1d046050c1b0037a8ea888eaa7ab6e69
el-tumero <tumerpl@gmail.com>
2025-01-07 00:08:14 +0100

    copy-addresses script fix

--- contracts/copy-addresses.sh
+++ contracts/copy-addresses.sh
@@ -1,8 +1,8 @@
 check_file="addresses-local.json"
-copy_file="addresses-local.json"
+copy_file="addresses-testnet.json"
 if [[ ! -f "$check_file" ]]; then
     echo "$check_file does not exist. Copying $copy_file to $check_file."
-    touch "$target_file"
+    cp $copy_file $check_file
 else
     echo "$check_file exists. No file created."
 fi
